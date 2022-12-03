32115cf3a334273880a25e9c65ed2d0666958887
Tyler <tyler.diep03@gmail.com>
2022-12-03 16:41:12 -0500

    updated build for lab page

--- src/App.js
+++ src/App.js
@@ -7,16 +7,13 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Welcome to t3diep's Lab 10 Part C! Using ReactJS Framework
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Hello
-        </a>
+    <p class="subtext">I installed this framework through the NodeJs website and following the page tutorial. 
+    I also had to download Git, so that I can upload this page to Github, and host it.</p>
+    <p class="subtext">Challenging Part: Following the tutorial to deploy this project to Github, since I had to use Git, which I have never used before. I was
+    having trouble understanding what each command did, but I was able to understand it through running them a bunch of times. The prompts from the command
+    were self explanatory. The most interesting part was to see how much Github provides and I believe I will be using this application more often in the future.</p>
       </header>
     </div>
   );
